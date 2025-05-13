5f9b054d3d09606eae8688cc8e70e2373a15d8c8
nikoladev06 <nikoladev06@gmail.com>
2025-05-13 10:52:51 -0300

    feat: Adicionado o desafio ListaDeTarefas
    Signed-off-by: nikoladev06 <nikoladev06@gmail.com>

---
+++ src/List/ListaDeTarefas/ListaTarefa.java
@@ -0,0 +1,52 @@
+package List.ListaDeTarefas;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    //atributo
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for(Tarefa t : tarefaList){
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
+    public int obterNumeroTotalDeTarefas(){
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefas(){
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalDeTarefas());
+
+        listaTarefa.adicionarTarefa("Tarefa 1: Quebrar o galho");
+        listaTarefa.adicionarTarefa("Tarefa 1: Quebrar o galho");
+        listaTarefa.adicionarTarefa("Tarefa 2: Quebrar o gelo");
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalDeTarefas());
+
+        listaTarefa.removerTarefa("Tarefa 2: Quebrar o gelo");
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalDeTarefas());
+
+        listaTarefa.obterDescricoesTarefas();
+    }
+
+
+}
