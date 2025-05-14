1772e669378e85a0b78c6d80de3623a4b186ad7f
nikoladev06 <nikoladev06@gmail.com>
2025-05-14 12:14:00 -0300

    feat: adicionado o desafio de ordenação CadastroProdutos
    Signed-off-by: nikoladev06 <nikoladev06@gmail.com>

--- src/Set/Ordenacao/CadastroProdutos.java
+++ src/Set/Ordenacao/CadastroProdutos.java
@@ -1,4 +1,52 @@
 package Set.Ordenacao;
 
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class CadastroProdutos {
+    private Set<Produto> produtoSet;
+
+    public CadastroProdutos() {
+        this.produtoSet = new HashSet<>();
+    }
+
+    public void adicionarProduto(long codigo, String nome, double preco, int quantidade) {
+        produtoSet.add(new Produto(codigo, nome, preco, quantidade));
+    }
+
+    public Set<Produto> exibirProdutosPorNome() {
+        Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+        return produtosPorNome;
+        //poderia só retornar new TreeSet<>(produtoSet);
+    }
+
+    public Set<Produto> exibirPorPreco() {
+        Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
+        produtosPorPreco.addAll(produtoSet);
+        return produtosPorPreco;
+    }
+
+    public static void main(String[] args) {
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+        cadastroProdutos.adicionarProduto(1L, "Leite", 6.99d, 100);
+        cadastroProdutos.adicionarProduto(3L, "Refrigerante", 8.99d, 200);
+        cadastroProdutos.adicionarProduto(2L, "Suco", 4.99d, 50);
+        cadastroProdutos.adicionarProduto(2L, "Bala", 1.99d, 70);
+
+        System.out.println(cadastroProdutos.produtoSet);
+        System.out.println(cadastroProdutos.exibirProdutosPorNome());
+        System.out.println(cadastroProdutos.exibirPorPreco());
+
+    }
+
+}
+
+class ComparatorPorPreco implements Comparator<Produto> {
+
+    @Override
+    public int compare(Produto p1, Produto p2) {
+        return Double.compare(p1.getPreco(), p2.getPreco());
+    }
 }
--- src/Set/Ordenacao/Produto.java
+++ src/Set/Ordenacao/Produto.java
@@ -1,4 +1,60 @@
 package Set.Ordenacao;
 
-public class Produto {
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Produto implements Comparable<Produto> {
+    private long codigo;
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Produto(long codigo, String nome, double preco, int quantidade) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public int compareTo(Produto p) {
+        return nome.compareToIgnoreCase(p.getNome());
+    }
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Produto produto)) return false;
+        return getCodigo() == produto.getCodigo();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getCodigo());
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "codigo=" + codigo +
+                ", nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", quantidade=" + quantidade +
+                '}';
+    }
 }
